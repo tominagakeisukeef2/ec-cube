c2955bdb78b2b7566fbda2ec8cc0d44d838a5a92
tominagakeisukeef2 <tominaga.keisuke.ef2@jp.nssol.nipponsteel.com>
2025-03-03 17:26:37 +0900

    例外スロー時にログを出力するように修正

--- src/Eccube/Controller/Admin/Product/ProductClassController.php
+++ src/Eccube/Controller/Admin/Product/ProductClassController.php
@@ -461,6 +461,7 @@ class ProductClassController extends AbstractController
         try {
             return $qb->getQuery()->getSingleResult();
         } catch (NoResultException $e) {
+            $this->logger->error('No product found for id: ' . $id, ['exception' => $e]);
             return null;
         }
     }
